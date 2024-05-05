f59c03d63b6ddce0d30500b9086dc34f585e8c7e
ellachamb <ellachamb@gmail.com>
2024-05-04 19:37:38 -0600

    week2

--- week1/controllers/index.js
+++
@@ -1,5 +0,0 @@
-const getName = (req, res) => {
-  res.send("Ella Chamberlain");
-};
-
-module.exports = { getName };
--- week1/routes/index.js
+++
@@ -1,6 +0,0 @@
-const routes = require("express").Router();
-const baseController = require("../controllers");
-
-routes.get("/", baseController.getName);
-
-module.exports = routes;
