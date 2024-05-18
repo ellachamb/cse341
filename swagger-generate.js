6c05278f14fd3790458cb26ccf452a150a5511ce
ellachamb <ellachamb@gmail.com>
2024-05-18 15:33:30 -0600

    swagger

--- swagger-generate.js
+++ swagger-generate.js
@@ -5,8 +5,8 @@ const doc = {
     title: "My API",
     description: "Contacts API",
   },
-  host: "cse341-1-802v.onrender.com",
-  schemes: ["https"],
+  host: "localhost:8080",
+  schemes: ["http"],
 };
 
 const outputFile = "./swagger.json";
