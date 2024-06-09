f362447a2b7d73fa0b6198b00ba29b64e835d4c1
ellachamb <ellachamb@gmail.com>
2024-06-08 19:19:58 -0600

    auth

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
-const { auth, requiresAuth } = require('express-openid-connect');
 require('dotenv').config();
 
 const port = process.env.PORT || 8080;
